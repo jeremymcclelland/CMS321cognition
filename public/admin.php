ce5c18c62f70a97ec4cfd99de1d1679bcec7d195
Jeremy McClelland <jeremy@moraycreative.com>
2015-10-28 13:48:59 -0400

    CSS added
    dropped in pre fab css. Will update at end to make responsive and
    mobile friendly

--- public/admin.php
+++ public/admin.php
@@ -3,26 +3,27 @@
 
 <html lang="en">
 	<head>
-		<title>untitled</title>
+	  <title>untitled</title>
+          <link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-    <div id="header">
-      <h1>321Cognition</h1>
-    </div>
-    <div id="main">
-      <div id="navigation">
-        &nbsp;
-      </div>
-      <div id="page">
-        <h2>Admin Menu</h2>
-        <p>Welcome to the admin area.</p>
-        <ul>
-          <li><a href="manage_content.php">Manage Website Content</a></li>
-          <li><a href="manage_admins.php">Manage Admin Users</a></li>
-          <li><a href="logout.php">Logout</a></li>
-        </ul>
-      </div>
-    </div>
-    <div id="footer">Copyright 20xx, 321Cognition</div>
+            <div id="header">
+              <h1>321Cognition</h1>
+            </div>
+            <div id="main">
+              <div id="navigation">
+                &nbsp;
+              </div>
+              <div id="page">
+                <h2>Admin Menu</h2>
+                <p>Welcome to the admin area.</p>
+                <ul>
+                  <li><a href="manage_content.php">Manage Website Content</a></li>
+                  <li><a href="manage_admins.php">Manage Admin Users</a></li>
+                  <li><a href="logout.php">Logout</a></li>
+                </ul>
+              </div>
+            </div>
+            <div id="footer">Copyright 20xx, 321Cognition</div>
 	</body>
 </html>
