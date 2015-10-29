db97f66673cc2992e93b4179ffdb1d39e010d9a5
Jeremy McClelland <jeremy@moraycreative.com>
2015-10-29 12:44:59 -0400

    refactoring navigation

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,53 +1,27 @@
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
-
 <?php include("../includes/layouts/header.php"); ?>
 
+<?php
+    if (isset($_GET["subject"])) {
+        $selected_subject_id = $_GET["subject"];
+        $selected_page_id = null;
+    } elseif (isset($_GET["page"])) {
+        $selected_page_id = $_GET["page"];
+        $selected_subject_id = null;
+    } else {
+        $selected_page_id = null;
+        $selected_subject_id = null;
+    }
+?>
 <div id="main">
     <div id="navigation">
-        <ul class="subjects">
-            <?php
-            $query = "SELECT * ";
-            $query .= "FROM subjects ";
-            $query .= "WHERE visible = 1 ";
-            $query .= "ORDER BY position ASC";
-            $subject_set = mysqli_query($connection, $query);
-            confirm_query($subject_set);
-            ?>
-            <?php
-            while ($subject = mysqli_fetch_assoc($subject_set)) {
-                ?>
-                <li>
-                    <?php echo $subject["menu_name"]; ?>
-                    <?php
-                    $query = "SELECT * ";
-                    $query .= "FROM pages ";
-                    $query .= "WHERE visible = 1 ";
-                    $query .= "AND subject_id = {$subject["id"]} ";
-                    $query .= "ORDER BY position ASC";
-                    $page_set = mysqli_query($connection, $query);
-                    confirm_query($page_set);
-                    ?>
-                    <ul class="pages">
-                    <?php
-                        while ($page = mysqli_fetch_assoc($page_set)) {
-                    ?>
-                    <li><?php echo $page["menu_name"]; ?></li>
-                    <?php
-                    }
-                    ?>
-                    <?php mysqli_free_result($page_set); ?>
-                    </ul>
-                </li>
-            <?php
-            }
-            ?>
-        </ul>
-        <?php mysqli_free_result($subject_set); ?>
+        <?php echo navigation($selected_subject_id, $selected_page_id); ?>
     </div>
     <div id="page">
         <h2>Manage Content</h2>
-
+        <?php echo $selected_subject_id; ?><br />
+        <?php echo $selected_page_id; ?>
     </div>
 </div>
 
